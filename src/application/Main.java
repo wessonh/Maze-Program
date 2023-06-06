583a3cbd14c749f664905d5b5b9c17b6d10dccd2
Michael Woo <michaelwoo36@gmail.com>
2023-06-05 20:42:10 -0700

    Merged condeser branch

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,37 +13,16 @@ public class Main extends Application {
 
     static final Group group = new Group(); // group to store nodes for scene display
     static Node[][] maze;
-
+    static Stage stage;
     @Override // entry point for javaFX
     public void start(Stage primaryStage) {
 
         try {
+      	  	stage = primaryStage;
             Scene scene = new Scene(group, 425, 425); // creates a new scene with group as the root, this is where you adjust window size
-            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Untitled.fxml")).toExternalForm()); // adds the CSS style sheet
-            primaryStage.setScene(scene); // sets primary stage to our scene
-            primaryStage.show(); // displays the window
-
-            maze = Gen.create(15, 15); // creates maze, sets rows and columns to value
-
-            Timeline timeline = new Timeline();// creates new timeline
-            int delay = 0; // line visibility delay set to zero
-
-            
-            for (javafx.scene.Node node : group.getChildren()) { // for loop iterates through every node in group
-
-                if (node instanceof Line line) { // if the current node is an instanceof line
-                    // adds new frame to timeLine
-                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(delay), e -> line.setVisible(true)));
-                    delay += 5; // sets the delay to 5 or whatever value.
-                }
-            }
-            timeline.setOnFinished(e -> { // when maze generation animation is finished.
-                Solve solve = new Solve(); // new solve
-                solve.search(maze);  // run the solution search
-                solve.animate(); // animate solution
-            });
-            timeline.play(); // starts animation
-
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("application.css")).toExternalForm()); // adds the CSS style sheet
+            stage.setScene(scene); // sets primary stage to our scene
+            stage.show(); // displays the window
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -51,9 +30,12 @@ public class Main extends Application {
 
     public static void main(String[] args) {
    	 
-   	         // sets rows and columns of maze
-        
+   	 Gen generator = new Gen();
+   	 maze = generator.create(15, 15);
+   	 Solve solver = new Solve();
+   	 solver.start(stage, group, maze);
    	 launch(args);
+   	 
+   	  // creates maze, sets rows and columns to value
     }
 }
-
--- src/application/Solve.java
+++ src/application/Solve.java
@@ -1,14 +1,52 @@
 package application;
+import java.util.Objects;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Solve {
 
-    private final Animations animations;
+    private Animations animations;
     private int delay;
 
     public Solve() {
 
         this.animations = new Animations(); // makes new animation object
         this.delay = 0; // sets delay to O
+    }
+
+    public void start(Stage stage, Group group, Node[][]maze) {
+
+        try {
+      	  	
+            Timeline timeline = new Timeline();// creates new timeline
+            int delay = 0; // line visibility delay set to zero
+
+            
+            for (javafx.scene.Node node : group.getChildren()) { // for loop iterates through every node in group
+
+                if (node instanceof Line line) { // if the current node is an instanceof line
+                    // adds new frame to timeLine
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(delay), e -> line.setVisible(true)));
+                    delay += 5; // sets the delay to 5 or whatever value.
+                }
+            }
+            timeline.setOnFinished(e -> { // when maze generation animation is finished.
+                 // new solve
+                search(maze);  // run the solution search
+                animate(); // animate solution
+            });
+            timeline.play(); // starts animation
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void search(Node[][] maze) {
@@ -36,7 +74,8 @@ public class Solve {
             }
             else {
                 for(Node neighbor : current.getNeighbors()) {
-                    if(neighbor.isVisited()) {  // check if the neighbor is unvisited
+                    if(
+                  		neighbor.isVisited()) {  // check if the neighbor is unvisited
                         // If neighbor is above current node and no walls are blocking way, visit and add to queue
                         if(neighbor.getRow() < current.getRow() && !neighbor.isDown() && !current.isUp()) {
                             neighbor.setVisited(true);
@@ -64,7 +103,6 @@ public class Solve {
     }
 
     public void animate(){ // method for adding frame to animation with chosen delay, animates the breadth first search
-       
         animations.playAnimation();
     }
 }
