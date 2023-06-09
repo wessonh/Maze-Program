8c4507ca389934839d66d7898514d55fb41aaf57
wessonh <80785364+wessonh@users.noreply.github.com>
2023-06-08 17:01:19 -0700

    Update Animations.java

--- src/application/Animations.java
+++ src/application/Animations.java
@@ -1,38 +1,34 @@
 package application;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
-
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Animations {
 
     private final Timeline timeline;
- 
+
     public Animations() {
 
-   	 this.timeline = new Timeline(); // new Timeline
+       this.timeline = new Timeline(); // new Timeline
     }
     public static void animateLines(List<Line> lines) {
 
-        // sets each line to be invisible at start
-        for (Line line : lines) {
+        for (Line line : lines) { // for loop sets lines to be invisible
             line.setVisible(false);
         }
 
         Timeline timeline = new Timeline(); // creates timeline for animateLines
-        int[] index = new int[1]; // uses int array to hold integer. This is because keyframe
-                                  // won't let us use a simple integer because it uses a lambda function.
+        AtomicInteger index = new AtomicInteger(); // Create a variable to keep track of the current index
         KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.05), e -> { // creates frame for animation
 
-            Line line = lines.get(index[0]); // get line at current index
+            Line line = lines.get(index.get()); // get line at current index
             line.setVisible(true); // make the line visible
-            index[0]++; // increment the index for next frame
+            index.getAndIncrement(); // increment the index
         });
 
         timeline.getKeyFrames().add(keyFrame);// Add key frame to the timeline
@@ -44,16 +40,16 @@ public class Animations {
         timeline.getKeyFrames().add(new KeyFrame(Duration.millis(delay), e -> { // adds nodes from solver as frames
             // marks visited with *
             Text text = new Text(current.getCol() * 25 + 37.5, current.getRow() * 25 + 37.5, "*");
-            // for nodes in group, add * to display
-            
-            Main.group2.getChildren().add(text); // for nodes in group, add * to display
+            Main.group.getChildren().add(text); // for nodes in group, add * to display
         }));
     }
 
-    public void playAnimation(Button button) {
+    public Timeline getTimeline() {
+        return this.timeline;
+    }
+
+    public void playAnimation() {
         timeline.play(); // play animation
-        timeline.setOnFinished(event -> {
-      	  button.setVisible(true);
-        });
+
     }
 }
