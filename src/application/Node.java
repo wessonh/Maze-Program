e5c3816327e5f0bb9e844b70e66502757de2ece1
wessonh <80785364+wessonh@users.noreply.github.com>
2023-06-08 17:04:04 -0700

    Update Node.java

--- src/application/Node.java
+++ src/application/Node.java
@@ -1,9 +1,8 @@
 package application;
 import java.util.ArrayList;
-import java.util.*;
 
-public class Node {
-    
+public class Node extends javafx.scene.Node {
+
     private boolean up = true; //booleans for the walls around nodes
     private boolean down = true;
     private boolean right = true;
@@ -12,22 +11,14 @@ public class Node {
     private boolean end;// boolean to check for end of the maze
     private final int row; // nodes position on maze grid
     private final int col;
-    private final ArrayList<Node> neighbors = new ArrayList<>(); // list stores all the neighboring node
-    public Integer distance = 0;
-    
-    Node parent;
-	 
+    private final ArrayList<Node> neighbors = new ArrayList<>(); // list stores all the neighboring nodes
 
-    // constructor for Node
-    public Node(int row, int col) {
+    public Node(int row, int col) { // constructor for Node
         this.visited = false;
         this.col = col;
         this.row = row;
         this.end = false;
-        
     }
-    
-    
 
     // getters and setters for the class variables
     public boolean isUp() {
@@ -104,6 +95,6 @@ public class Node {
 
     // adds neighbors.
     public void addNeighbor(Node node) {
-       this.neighbors.add(node);
-   }
+        this.neighbors.add(node);
+    }
 }
