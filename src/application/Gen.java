d319496f637a8e257b737d84b0fbb8a73ccf3086
wessonh <80785364+wessonh@users.noreply.github.com>
2023-06-11 12:40:32 -0700

    Update Gen.java

--- src/application/Gen.java
+++ src/application/Gen.java
@@ -45,7 +45,9 @@ public class Gen {
         print(row, col, maze); // Calls the print method to print the generated maze.
         return maze;
     }
+
     private static void generateMaze(int row, int col, Node[][] maze) {
+
         // makes stack to keep track of visited nodes
         Stack<Node> stack = new Stack<>();
         // START NODE. sets the start node as top left. we might want to let the user able to adjust this
@@ -71,7 +73,7 @@ public class Gen {
                 Node chosen = unvisitedNeighbors.get((int) (Math.random() * unvisitedNeighbors.size()));
                 chosen.setVisited(true); // mark as visited
                 stack.push(current);
-                
+
                 stack.push(chosen);
 
                 // determines direction of the chosen neighbor
@@ -100,6 +102,7 @@ public class Gen {
         end.setDown(false);
         end.setEnd(true);
     }
+
     private static void print(int row, int col, Node[][] maze) {
 
         List<Line> visibleLines = new ArrayList<>();// Create list for visible lines and all visited nodes
@@ -170,14 +173,12 @@ public class Gen {
         }
         timeline.play();
         timeline.setOnFinished(event -> {
-      	  
-      	  
-      	  Main.back.setDisable(false);
-      	  Main.clearButton.setDisable(false);
-      	  Main.solveButton.setDisable(false);
-      	  Main.saveButton.setDisable(false);
-      	  
-      	  
+
+            Main.back.setDisable(false);
+            Main.clearButton.setDisable(false);
+            Main.solveButton.setDisable(false);
+            Main.saveButton.setDisable(false);
+
         });
         return maze;
     }
@@ -185,5 +186,28 @@ public class Gen {
     public void render(int row, int col, Node[][] maze) {
 
         print(row, col, maze); // Calls the print method to print the maze.
+    }
+
+    public static void animateLoadedMaze(Group group, Node[][] maze, int row, int col) {
+        print(row, col, maze); // Calls the print method to print the maze.
+
+        Timeline timeline = new Timeline();
+        int delay = 0;
+
+        for (javafx.scene.Node node : group.getChildren()) {
+            if (node instanceof Line line) {
+                timeline.getKeyFrames().add(new KeyFrame(Duration.millis(delay), e -> line.setVisible(true)));
+                delay += 5;
+            }
+        }
+        timeline.play();
+        timeline.setOnFinished(event -> {
+
+            Main.back.setDisable(false);
+            Main.clearButton.setDisable(false);
+            Main.solveButton.setDisable(false);
+            Main.saveButton.setDisable(false);
+
+        });
     }
 }
