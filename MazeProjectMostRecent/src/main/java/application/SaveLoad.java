91b6a7cbad5d1e6d24fbe4b622e7f6f4714494b6
wessonh <80785364+wessonh@users.noreply.github.com>
2023-06-12 14:59:36 -0700

    Update SaveLoad.java
    added comments

--- MazeProjectMostRecent/src/main/java/application/SaveLoad.java
+++ MazeProjectMostRecent/src/main/java/application/SaveLoad.java
@@ -9,21 +9,24 @@ public class SaveLoad {
     private static Stage stage;
 
     public SaveLoad(Stage stage) {
+        
         SaveLoad.stage = stage;
     }
 
     public static void saveFile(Node[][] maze, String fileName) {
+        
         try {
             FileWriter fileWriter = new FileWriter(fileName);
             PrintWriter printWriter = new PrintWriter(fileWriter);
-            int rows = maze.length;
-            int cols = maze[0].length;
-            printWriter.println(rows);
-            printWriter.println(cols);
+            int rows = maze.length; // get rows in maze
+            int cols = maze[0].length; // get columns in maze
+            printWriter.println(rows); // write rows to file
+            printWriter.println(cols); // cols to file
 
-            for (int i = 0; i < rows; i++) {
+            for (int i = 0; i < rows; i++) { // loop through all nodes in maze
                 for (int j = 0; j < cols; j++) {
-                    Node node = maze[i][j];
+                    Node node = maze[i][j]; // get node at i, j
+                    // writes node to the text file
                     printWriter.println(node.getRow() + "," + node.getCol() + "," +
                             node.isUp() + "," + node.isDown() + "," +
                             node.isLeft() + "," + node.isRight() + "," +
@@ -37,7 +40,7 @@ public class SaveLoad {
         }
     }
 
-    public static void save(Node[][] maze) {
+    public static void save(Node[][] maze) { // Opens file chooser and saves the maze
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Save Maze");
@@ -49,21 +52,23 @@ public class SaveLoad {
         }
     }
 
-    public static Node[][] loadFile(String fileName) {
+    public static Node[][] loadFile(String fileName) { // loads maze from text file
+
         Node[][] maze = null;
+
         try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            int rows = Integer.parseInt(bufferedReader.readLine());
-            int cols = Integer.parseInt(bufferedReader.readLine());
-            Main.x = cols;
-            Main.y = rows;
-            maze = new Node[rows][cols];
+            int rows = Integer.parseInt(bufferedReader.readLine()); // reads rows from file
+            int cols = Integer.parseInt(bufferedReader.readLine()); // reads cols from file
+            Main.x = cols; // sets x for Main
+            Main.y = rows; // sets y for Main
+            maze = new Node[rows][cols]; // initialize maze
 
-            for (int i = 0; i < rows; i++) {
+            for (int i = 0; i < rows; i++) { // loops every nodes in maze
                 for (int j = 0; j < cols; j++) {
                     String[] data = bufferedReader.readLine().split(",");
-                    int row = Integer.parseInt(data[0]);
+                    int row = Integer.parseInt(data[0]); // parses row, col, up, down, left, right, visited, and end
                     int col = Integer.parseInt(data[1]);
                     boolean up = Boolean.parseBoolean(data[2]);
                     boolean down = Boolean.parseBoolean(data[3]);
@@ -72,7 +77,7 @@ public class SaveLoad {
                     boolean visited = Boolean.parseBoolean(data[6]);
                     boolean end = Boolean.parseBoolean(data[7]);
 
-                    Node node = new Node(row, col);
+                    Node node = new Node(row, col); // creates new node and sets the data
                     node.setUp(up);
                     node.setDown(down);
                     node.setLeft(left);
@@ -80,30 +85,31 @@ public class SaveLoad {
                     node.setVisited(visited);
                     node.setEnd(end);
 
-                    maze[i][j] = node;
+                    maze[i][j] = node; // set node in maze
                 }
             }
 
             // when all nodes are loaded, remake the neighbors list
-            for (int i = 0; i < rows; i++) {
+            for (int i = 0; i < rows; i++) { // loops through every node in maze
                 for (int j = 0; j < cols; j++) {
                     ArrayList<Node> neighbors = new ArrayList<>();
-                    if(i > 0) neighbors.add(maze[i-1][j]); // up
-                    if(j > 0) neighbors.add(maze[i][j-1]); // left
-                    if(i < rows - 1) neighbors.add(maze[i+1][j]); // down
-                    if(j < cols - 1) neighbors.add(maze[i][j+1]); // right
-                    maze[i][j].setNeighbors(neighbors);
+                    if(i > 0) neighbors.add(maze[i-1][j]); // add node top if exists
+                    if(j > 0) neighbors.add(maze[i][j-1]); // add node left if exists
+                    if(i < rows - 1) neighbors.add(maze[i+1][j]); // add node below if exists
+                    if(j < cols - 1) neighbors.add(maze[i][j+1]); // add node right if exists
+                    maze[i][j].setNeighbors(neighbors); // sets the neighbors of node
                 }
             }
             bufferedReader.close();
-            System.out.println("Maze loaded successfully: " + fileName);
+            System.out.println("Maze loaded successfully: " + fileName); // success message
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return maze;
+        return maze; // return loaded maze
     }
 
-    public static Node[][] load() {
+    public static Node[][] load() { // opens file chooser and loads maze
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Load Maze");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
